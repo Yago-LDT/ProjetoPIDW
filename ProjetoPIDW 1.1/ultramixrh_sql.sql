4d30f40c1fc14467ed7cc8dcea8570de3f8c375b
Yago Pereira <yagopereiranunes6@gmail.com>
2024-11-14 01:13:38 -0300

    Add files via upload

---
+++ ProjetoPIDW 1.1/ultramixrh_sql.sql
@@ -0,0 +1,112 @@
+-- DROP SCHEMA `ultramixrh` ;
+
+CREATE SCHEMA IF NOT EXISTS `ultramixrh` DEFAULT CHARACTER SET utf8mb4 ;
+USE `ultramixrh` ;
+
+create table if not exists login (id int not null auto_increment primary key, usuario varchar(50), senha varchar(255));
+
+select * from login;
+
+insert into login (usuario, senha) values('admin', '123456');
+
+CREATE TABLE IF NOT EXISTS funcionarios (
+	id INT(11) NOT NULL AUTO_INCREMENT,
+	nome VARCHAR(50),
+  `cargo` VARCHAR(40),
+  `salario` DOUBLE,
+  `CPF` INT(11),
+  `data_admissão` DATE,
+  `telefone` VARCHAR(14),
+  `email` VARCHAR(50),
+  PRIMARY KEY (`id`));
+
+
+CREATE TABLE IF NOT EXISTS avaliacoes(
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `funcionario_id` INT(11) NOT NULL,
+  `produtividade` VARCHAR(100),
+  `empenho` VARCHAR(100),
+  `relatorio` VARCHAR(500),
+  `recomenda_promoção` VARCHAR(20),
+  PRIMARY KEY (`id`),
+    FOREIGN KEY (`funcionario_id`)
+    REFERENCES `ultramixrh`.`funcionarios` (`id`));
+
+
+CREATE TABLE IF NOT EXISTS `ultramixrh`.`banco_horas` (
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `funcionario_id` INT(11) NOT NULL,
+  `horas_em_banco` INT(11),
+  `ferias` VARCHAR(40),
+  `licencas` VARCHAR(40),
+  PRIMARY KEY (`id`),
+    FOREIGN KEY (`funcionario_id`)
+    REFERENCES `ultramixrh`.`funcionarios` (`id`));
+
+
+CREATE TABLE IF NOT EXISTS `ultramixrh`.`cargos` (
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `titulo` VARCHAR(40),
+  `carga_horaria` INT(11),
+  `funcao` VARCHAR(300),
+  `faixa_salarial` VARCHAR(50),
+  PRIMARY KEY (`id`));
+
+
+CREATE TABLE IF NOT EXISTS `ultramixrh`.`fornecedores` (
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `nome` VARCHAR(50),
+  `produto` VARCHAR(50),
+  `CNPJ` INT(14),
+  `telefone` VARCHAR(14),
+  `email` VARCHAR(50),
+  `ultima_remessa` DATE,
+  `proxima_remessa` DATE,
+  PRIMARY KEY (`id`));
+
+
+
+CREATE TABLE IF NOT EXISTS `ultramixrh`.`contratos` (
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `fornecedor_id` INT(11) NOT NULL,
+  `duracao` VARCHAR(20),
+  `produto_quantidade` VARCHAR(30),
+  `custos` VARCHAR(20),
+  PRIMARY KEY (`id`),
+    FOREIGN KEY (`fornecedor_id`)
+    REFERENCES `ultramixrh`.`fornecedores` (`id`));
+
+
+
+CREATE TABLE IF NOT EXISTS `ultramixrh`.`folha_pagamento` (
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `funcionario_id` INT(11) NOT NULL,
+  `cargo_id` INT(11) NOT NULL,
+  `salario_bruto` DOUBLE,
+  `beneficios` INT(11),
+  `bonus` INT(11),
+  `valor_receber` INT(11),
+  PRIMARY KEY (`id`),
+    FOREIGN KEY (`funcionario_id`)
+    REFERENCES `ultramixrh`.`funcionarios` (`id`),
+    FOREIGN KEY (`cargo_id`)
+    REFERENCES `ultramixrh`.`cargos` (`id`));
+
+CREATE TABLE IF NOT EXISTS `ultramixrh`.`folha_ponto` (
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `funcionario_id` INT(11) NOT NULL,
+  `horario_chegada` DATETIME,
+  `horario_saida` DATETIME,
+  `horas` TIME GENERATED ALWAYS AS (timediff(`horario_saida`,`horario_chegada`)) STORED,
+  PRIMARY KEY (`id`),
+    FOREIGN KEY (`funcionario_id`)
+    REFERENCES `ultramixrh`.`funcionarios` (`id`));
+
+
+CREATE TABLE IF NOT EXISTS `ultramixrh`.`funcionario_cargo` (
+  `funcionario_id` INT(11) NOT NULL,
+  `cargo_id` INT(11) NOT NULL,
+    FOREIGN KEY (`funcionario_id`)
+    REFERENCES `ultramixrh`.`funcionarios` (`id`),
+    FOREIGN KEY (`cargo_id`)
+    REFERENCES `ultramixrh`.`cargos` (`id`));
---
+++ ProjetoPIDW 1.1/visao/formularios/funcionarios/listarfuncionario.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Funcionário UltraMix RH</title>
+  <link rel="stylesheet" href="./../../css/form.css">
+</head>
+<body>
+    <div class="login-container">
+        <div class="login-box">
+            <div class="logo-container">
+                <img src="./../../img/Logotipo_moderno_minimalista_azul_marinho_para_ecommerce-removebg-preview.png" alt="logo" class="logo">
+            </div>
+            
+            
+            <?php
+    require './.././../../modelo/DAO/ClassUsuarioDAO.php';
+
+    $classUsuarioDAO = new ClassUsuarioDAO();
+    $us = $classUsuarioDAO->listarfuncionarios();
+
+    foreach ($us as $us) {
+      echo "<div>";
+    	echo "<table>";
+      echo "<tr>";
+      echo "<td><p>" . $us['id'] . "</p></td>";
+      echo "<td><p>" . $us['nome'] . "</p></td>";
+      echo "<td><p>" . $us['cargo'] . "</p></td>";
+      echo "<td><p>" . $us['salario'] . "</p></td>";
+      echo "<td><p>" . $us['data_admissão'] . "</p></td>";
+      echo "<td><p>" . $us['telefone'] . "</p></td>";
+      echo "<td><p>" . $us['email'] . "</p></td>";
+      echo "</tr>"; 
+      echo "</table>";
+      echo "<div>";
+
+    }
+
+?>
+<br><br>
+<button type="button" class="btn-submit" onclick="window.location.href='../../../menu.html'">Voltar</button>
+            
+            <div id="error-message" class="error-message"></div>
+
+            </div>
+
+        </div>
+    </div>
+    <script src=""></script>
+    </body>
+    </html>
